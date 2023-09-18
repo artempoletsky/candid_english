9ac3b994e58605e107b8c000e7fe485d7c92669b
Artem Poletsky <artempoletsky@gmail.com>
2023-09-18 23:31:19 +0500

    blog init

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function BlogMain() {
+  return(
+    <div className="">
+      <h1 className="foo">Welcome</h1>
+      <Link href="/">Back to the main page</Link>
+    </div>
+  );
+};
