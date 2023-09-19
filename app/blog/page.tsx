d45168d60740fda7c3a205e61d52de480cc584c6
Artem Poletsky <artempoletsky@gmail.com>
2023-09-19 14:07:20 +0500

    tutorial progress: module css

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import Link from 'next/link';
+import styles from './custom.module.css';
 
 export const metadata: Metadata = {
   title: 'Blog',
@@ -8,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function BlogMain() {
   return(
-      <div className="">
+      <div className={styles.container}>
         <h1 className="foo">Welcome</h1>
         <Link href="/">Back to the main page</Link>
       </div>
