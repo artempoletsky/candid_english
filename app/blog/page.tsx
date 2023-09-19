3ee5f06c1949a3c8947ca0089c80d5486453e5c0
Artem Poletsky <artempoletsky@gmail.com>
2023-09-19 13:48:17 +0500

    progress

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,16 @@
-import Link from 'next/link'
+import { Metadata } from 'next';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'A blog about English language.',
+};
 
 export default function BlogMain() {
   return(
-    <div className="">
-      <h1 className="foo">Welcome</h1>
-      <Link href="/">Back to the main page</Link>
-    </div>
+      <div className="">
+        <h1 className="foo">Welcome</h1>
+        <Link href="/">Back to the main page</Link>
+      </div>
   );
 };
