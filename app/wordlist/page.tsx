649e4f7d48d627ac298c3958b253f01dc26d3c21
Artem Poletsky <artempoletsky@gmail.com>
2023-09-20 17:18:45 +0500

    filter words by list

--- app/wordlist/page.tsx
+++ app/wordlist/page.tsx
@@ -5,27 +5,23 @@ import WordList from './wordlist';
 
 import fs from 'fs'
 
-const data = JSON.parse(fs.readFileSync("./data/words_light.json", { encoding: "utf8" }));
+const data = JSON.parse(fs.readFileSync("./grab_data/words_light.json", { encoding: "utf8" }));
 
 // import styles from './custom.module.css';
 
 export const metadata: Metadata = {
-  title: 'Test',
-  description: 'Start testing your English level',
+  title: 'Explore Oxford\'s 5000 wordlist',
+  description: 'Explore Oxford Learner\'s Word List',
 }
 
 
 let level: string = 'any';
 
-
-
 export default function BlogMain() {
   return (
     <div className="">
-
       <h1 className="foo">Wordlist</h1>
       <WordList data={data}></WordList>
-
       <Link href="/">Back to the main page</Link>
     </div>
   );
--- app/wordlist/wordlist.tsx
+++ app/wordlist/wordlist.tsx
@@ -6,7 +6,8 @@ type Word = {
   id: string,
   word: string,
   level: string,
-  part: string
+  part: string,
+  use_in_test: boolean
 };
 
 
@@ -15,6 +16,8 @@ export default function WordList({ data }: { data: Array<Word> }) {
   let [level, setLevel] = useState("any");
   let [part, setPart] = useState("any");
   let [searchQuery, setSearchQuery] = useState("");
+  let [excludedWords, setExcludedWords] = useState<Array<String>>([]);
+  let testEditMode = true;
 
   const levelOptions: { [key: string]: string } = {
     any: 'Any',
@@ -35,35 +38,62 @@ export default function WordList({ data }: { data: Array<Word> }) {
   };
 
   const levelOptionsArr = Object.keys(levelOptions)
-    .map(key => <option value={key}>{levelOptions[key]}</option>);
+    .map(key => <option key={key} value={key}>{levelOptions[key]}</option>);
 
   const partOptionsArr = Object.keys(partOptions)
-    .map(key => <option value={key}>{partOptions[key]}</option>);
+    .map(key => <option key={key} value={key}>{partOptions[key]}</option>);
 
   const filterFn = (e: Word) => {
     if (level != "any" && level != e.level) return false;
     if (part != "any" && part != e.part) return false;
     if (searchQuery != "" && !e.word.startsWith(searchQuery)) return false;
+    if (excludedWords.includes(e.word)) return false;
     return true;
   };
 
+  const words = data.filter(filterFn);
+  const wordCount = words.length;
   return (
     <>
-      <label htmlFor="level_select">Level: </label>
-      <select id="level_select" value={level} onChange={e => setLevel(e.target.value)}>
-        {levelOptionsArr}
-      </select>
-      <label htmlFor="part_select">Part of speech: </label>
-      <select id="part_select" value={part} onChange={e => setPart(e.target.value)}>
-        {partOptionsArr}
-      </select>
-      <label htmlFor="search_q">Search: </label>
-      <input id="search_q" value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />
+      <div className="flex">
+        <label htmlFor="level_select">Level: </label>
+        <select id="level_select" value={level} onChange={e => setLevel(e.target.value)}>
+          {levelOptionsArr}
+        </select>
+        <label htmlFor="part_select">Part of speech: </label>
+        <select id="part_select" value={part} onChange={e => setPart(e.target.value)}>
+          {partOptionsArr}
+        </select>
+        <label htmlFor="search_q">Search: </label>
+        <input id="search_q" value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />
+      </div>
+      <label htmlFor="filter_file">Exclude words from list:</label>
+      <input id="filter_file" type="file" onChange={e => {
+        const files = e.target.files;
+        if (!files) return;
+        const file = files[0];
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          const text: String = e.target!.result as String;
+          setExcludedWords(text.split("\n").slice(2).map(line => line.split(/[\s;,]/)[0]));
+        }
+        reader.readAsText(file);
+      }} />
+      <button className="txt-vol rounded-xl" onClick={() => {
+        let input: HTMLInputElement = document.getElementById("filter_file") as HTMLInputElement;
+        input.value = "";
+        setExcludedWords([]);
+      }}>Clear</button>
+      Word count: {wordCount}
       <div className={css.container}>
         <table className={css.table}>
           <tbody>
-            {data.filter(filterFn).map((el: Word) =>
-              <tr>
+            {words.map((el: Word) =>
+              <tr key={el.id}>
+                {testEditMode ?
+                  <td>
+                    <input type='checkbox' checked={el.use_in_test} onChange={e => el.use_in_test = e.target.checked} />
+                  </td> : ''}
                 <td>
                   <a className={css.word} target="_blank" href={"https://www.oxfordlearnersdictionaries.com/definition/english/" + el.id}>{el.word}</a>
                 </td>
