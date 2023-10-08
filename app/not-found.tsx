5aaa558c3963893f50b8d49d6622c8ef5fad400b
Artem Poletsky <artempoletsky@gmail.com>
2023-10-08 14:15:25 +0500

    added custom 404

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col justify-center text-center absolute inset-0">
+      <h1 className="mb-0 text-9xl">404</h1>
+      <p>Nothing is here</p>
+    </div>
+  );
+}
